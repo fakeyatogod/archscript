49af42e06d6c620922859494439472d346f7af18
Yato <fakeyatokami@gmail.com>
2021-04-04 15:57:35 +0530

    Update b.sh

--- b.sh
+++ b.sh
@@ -27,4 +27,5 @@ systemctl enable NetworkManager
 
 pacman -S pacman-contrib
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
