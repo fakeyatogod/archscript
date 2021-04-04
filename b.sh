11bb43e73db6425a953b8a8e575d25783dc211b6
Yato <fakeyatokami@gmail.com>
2021-04-04 15:03:31 +0530

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,25 @@
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+
+pacman -S nano
+nano /etc/locale.gen
+locale-gen
+nano /etc/hostname
+nano /etc/hosts
+
+passwd
+useradd -m yato
+passwd yato
+usermod -aG wheel,audio,video,optical,storage yato
+
+pacman -S sudo
+EDITOR=nano visudo
+
+pacman -S grub intel-ucode
+pacman -S  efibootmgr dosfstools os-prober mtools
+mkdir /boot/EFI
+mount /dev/sda1 /boot/EFI
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman -S networkmanager
+systemctl enable NetworkManager
