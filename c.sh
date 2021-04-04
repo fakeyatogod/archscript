1ae490df1dd33dc6069b7668d4ca4325f3cf6b95
Yato <fakeyatokami@gmail.com>
2021-04-04 15:36:00 +0530

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,9 @@
+sudo pacman -S xorg xorg-xinit
+
+sudo pacman -S xf86-video-intel mesa nvidia nvidia-utils
+
+sudo pacman -S xf86-input-synaptics
+
+sudo pacman -S gnome gnome-extra
+
+sudo systemctl enable gdm
